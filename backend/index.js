9de5c4f9fcd02005486843f75d3290c8ff98bdab
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2024-08-28 00:36:07 +0530

    cors fixed

--- backend/index.js
+++ backend/index.js
@@ -17,20 +17,20 @@ app.use(express.json());
 
 //--------------------------------------------------------------
 // Define allowed origins
-const allowedOrigins = ['https://baral-bakery-shop-7opc.vercel.app'];
+// const allowedOrigins = ['https://baral-bakery-shop-7opc.vercel.app'];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions)); // Enable CORS with options
+//app.use(cors(corsOptions)); // Enable CORS with options
 //-------------------------------------
 
 app.use('/api/users',userRoutes);
